f08f1861171bc3e54c0237d69e394dc15492b845
CodingBlow <mdfarhanraza001@gmail.com>
2025-04-30 11:24:52 +0530

    Updated

--- data/pet-names.js
+++ data/pet-names.js
@@ -10,35 +10,68 @@ const petNames = {
             "Willow", "Murphy", "Penny", "Roxy", "Oreo", "Maggie", "Angel", "Harley", "Diesel", "Bear",
             "Baxter", "Bentley", "Archie", "Blue", "Duke", "Ellie", "Emma", "Gracie", "George", "Izzy",
             "Jack", "Koda", "Lily", "Marley", "Maya", "Mia", "Millie", "Nova", "Ollie", "Piper",
-            "Sophie", "Tucker", "Wally", "Winnie", "Chloe", "Felix", "Thor", "Mocha", "Bandit", "Echo"
+            "Sophie", "Tucker", "Wally", "Winnie", "Chloe", "Felix", "Thor", "Mocha", "Bandit", "Echo",
+            // New additions
+            "Sammy", "Hazel", "Nala", "Gus", "Ivy", "Atlas", "Cleo", "Rusty", "Tessa", "Maverick",
+            "Jade", "Rex", "Sasha", "Boomer", "Poppy", "Frankie", "Violet", "Dexter", "Pippin", "Aspen",
+            "Tango", "Moose", "Cinnamon", "Basil", "Sable", "Mimi", "Zara", "Casper", "Lulu", "Bruno",
+            "Opal", "Titan", "Skye", "Bowie", "Fiona", "Ranger", "Ebony", "Phoebe", "Gideon", "Sienna",
+            "Dash", "Tinker", "Mabel", "Arrow", "Cairo", "Raven", "Juniper", "Odin", "Pearl", "Saffron"
         ],
         cute: [
             "Mochi", "Pumpkin", "Cupcake", "Button", "Jellybean", "Sprinkles", "Marshmallow", "Waffle", 
             "Cookie", "Biscuit", "Pudding", "Buttercup", "Candy", "Cotton", "Bubbles", "Pebbles", "Pookie", 
             "Snuggles", "Waffles", "Nugget", "Twinkle", "Sweetie", "Charm", "Skippy", "Honey", "Sugar", 
             "Cuddles", "Peaches", "Tiny", "Sparkle", "Puff", "Squeak", "Bunny", "Doodle", "Giggles", 
-            "Pipsqueak", "Bean", "Pixel", "Bitsy", "Dotty"
+            "Pipsqueak", "Bean", "Pixel", "Bitsy", "Dotty",
+            // New additions
+            "Muffin", "Toffee", "Lollipop", "Taffy", "Snickers", "Fudge", "Popcorn", "Caramel", "Skittles", 
+            "Gummy", "Petal", "Chiffon", "Tinkerbell", "Fluffel", "Daisy", "Bambi", "Sprout", "Churro", 
+            "Puffy", "Clementine", "Bouncy", "Tutu", "Glitter", "Smore", "Noodle", "Pinky", "Wiggly", 
+            "Bonbon", "Pippin", "Crumpet", "Sundae", "Tinsel", "Bloop", "Zuzu", "Pancake", "Munchkin", 
+            "Coco", "Fizzy", "Ducky", "Pebbles"
         ],
         cool: [
             "Ace", "Bolt", "Blaze", "Dash", "Maverick", "Axel", "Rebel", "Storm", "Raven", "Phoenix",
             "Ghost", "Arrow", "Hunter", "Onyx", "Viper", "Jett", "Ranger", "Rocket", "Razor", "Slate",
-            "Nitro", "Jax", "Steele", "Legend", "Bullet", "Rex", "Vortex", "Blade", "Asher", "Frost"
+            "Nitro", "Jax", "Steele", "Legend", "Bullet", "Rex", "Vortex", "Blade", "Asher", "Frost",
+            // New additions
+            "Drift", "Cruz", "Zane", "Falcon", "Sable", "Hawk", "Talon", "Surge", "Kaiser", "Rogue",
+            "Cobalt", "Zephyr", "Striker", "Dusk", "Gunner", "Stryker", "Vapor", "Shadow", "Blitz", 
+            "Canyon", "Rush", "Knox", "Haze", "Volt", "Jetstream", "Panther", "Orion", "Spear", "Drake",
+            "Torque", "Grit", "Saber", "Flash", "Echo", "Iron", "Wolf", "Shade", "Rift", "Mako"
         ],
         funny: [
             "Taco", "Noodle", "Pickles", "Sir Fluffington", "Captain Whiskers", "Professor Pawprints",
             "Pancake", "Nacho", "Burrito", "Potato", "Waffles", "Ketchup", "Mustard", "Meatball", "Sushi",
             "Pretzel", "Lord Barksalot", "Miss Furbottom", "Count Lickula", "Dr. Fluffles", "Sergeant Snugglekins",
-            "Lieutenant Fuzzyboots", "Princess Purrpants", "King Woofington", "Lady Whiskerface", "Sir Barksalot"
+            "Lieutenant Fuzzyboots", "Princess Purrpants", "King Woofington", "Lady Whiskerface", "Sir Barksalot",
+            // New additions
+            "Baron Von Meow", "General Furrypants", "Admiral Snugglebutt", "Duke Pawsington", "Mister Wiggles", 
+            "Doctor Purrfect", "Colonel Cuddles", "Squire Scratchalot", "Madame Floof", "Chewy McFluff", 
+            "Bark Twain", "Purrsephone", "Furry Poppins", "Meowzart", "Woofgang Amadeus", "Catniss Everclean", 
+            "Dogtor Strange", "Pawdrey Hepburn", "Bark Vader", "Furball Einstein", "Hairy Potter", 
+            "Paws Solo", "Munchkin McMuffin", "Chomperella", "Sir Lickington", "Fluffy McFluffface"
         ],
         classic: [
             "Rex", "Fido", "Spot", "Lady", "Rover", "Fluffy", "Boots", "Tiger", "Kitty", "Buddy",
             "Buster", "Princess", "Spike", "Rusty", "Duke", "Duchess", "King", "Queenie", "Champ", "Sandy",
-            "Lucky", "Goldie", "Pepper", "Ginger", "Blackie", "Snowball", "Midnight", "Misty", "Sammy", "Peanut"
+            "Lucky", "Goldie", "Pepper", "Ginger", "Blackie", "Snowball", "Midnight", "Misty", "Sammy", "Peanut",
+            // New additions
+            "Prince", "Scamp", "Trixie", "Patches", "Bandit", "Shadow", "Smokey", "Coco", "Dusty", "Socks",
+            "Muffin", "Brownie", "Puffy", "Bingo", "Scooter", "Tinker", "Mandy", "Benji", "Pinky", "Candy",
+            "Barney", "Molly", "Skippy", "Jasper", "Toby", "Rags", "Snickers", "Lassie", "Pal", "Bouncer",
+            "Star", "Honey", "Rufus", "Cinnamon", "Penny", "Snoopy", "Chico", "Milo", "Paddy", "Dolly"
         ],
         unique: [
             "Krypto", "Zephyr", "Indigo", "Nebula", "Odyssey", "Zenith", "Quantum", "Catalyst", "Vector", "Equinox",
             "Solstice", "Axiom", "Nimbus", "Zodiac", "Pixel", "Quasar", "Synth", "Cipher", "Orbit", "Echo",
-            "Gadget", "Helix", "Jupiter", "Kaleidoscope", "Labyrinth", "Meridian", "Oblivion", "Parallax", "Sphinx", "Vertex"
+            "Gadget", "Helix", "Jupiter", "Kaleidoscope", "Labyrinth", "Meridian", "Oblivion", "Parallax", "Sphinx", "Vertex",
+            // New additions
+            "Aether", "Cosmo", "Vellum", "Prism", "Aurora", "Vanguard", "Elysium", "Spectra", "Nexus", "Chronos",
+            "Zion", "Pulsar", "Vesper", "Galaxia", "Icarus", "Nova", "Eon", "Tempest", "Sirocco", "Argon",
+            "Lyric", "Ozone", "Flux", "Mirage", "Crimson", "Asteria", "Vortex", "Draco", "Saffron", "Rune",
+            "Umber", "Celestine", "Ignis", "Borealis", "Zest", "Cetus", "Lumen", "Arcadia", "Stratos", "Obsidian"
         ]
     },
     
@@ -50,21 +83,39 @@ const petNames = {
             "Beau", "Gus", "Koda", "Murphy", "Archie", "Bruno", "Ace", "Apollo", "Hank", "Henry",
             "Luna", "Bella", "Daisy", "Lucy", "Bailey", "Sadie", "Molly", "Stella", "Roxy", "Zoey",
             "Lily", "Lola", "Zoe", "Ruby", "Riley", "Ellie", "Nala", "Abby", "Chloe", "Pepper",
-            "Rosie", "Willow", "Piper", "Sophie", "Maggie", "Coco", "Gracie", "Harper", "Penny", "Sasha"
+            "Rosie", "Willow", "Piper", "Sophie", "Maggie", "Coco", "Gracie", "Harper", "Penny", "Sasha",
+            // New additions
+            "Rusty", "Sammy", "Hazel", "Boomer", "Tessa", "Frankie", "Maverick", "Cleo", "Poppy", "Jade",
+            "Rex", "Violet", "Dexter", "Pippin", "Aspen", "Moose", "Tango", "Cinnamon", "Sable", "Bruno",
+            "Zara", "Casper", "Lulu", "Opal", "Titan", "Skye", "Bowie", "Fiona", "Ranger", "Ebony",
+            "Phoebe", "Gideon", "Sienna", "Dash", "Tinker", "Mabel", "Arrow", "Cairo", "Raven", "Juniper",
+            "Odin", "Pearl", "Saffron", "Scout", "Ivy", "Atlas", "Nala", "Gus", "Mimi", "Zara"
         ],
         male: [
             "Max", "Buddy", "Charlie", "Cooper", "Jack", "Rocky", "Bear", "Duke", "Tucker", "Oliver",
             "Milo", "Bentley", "Zeus", "Louie", "Winston", "Toby", "Jax", "Oscar", "Finn", "Bandit",
             "Beau", "Gus", "Koda", "Murphy", "Archie", "Bruno", "Ace", "Apollo", "Hank", "Henry",
             "Rex", "Teddy", "Leo", "Dexter", "Diesel", "Rusty", "Atlas", "Thor", "Samson", "Oreo",
-            "Marley", "Simba", "Riley", "Tank", "Jasper", "Buster", "Baxter", "Scout", "Moose", "Harley"
+            "Marley", "Simba", "Riley", "Tank", "Jasper", "Buster", "Baxter", "Scout", "Moose", "Harley",
+            // New additions
+            "Ranger", "Boomer", "Frankie", "Pippin", "Titan", "Cairo", "Gideon", "Dash", "Odin", "Casper",
+            "Arrow", "Bruno", "Rex", "Sammy", "Maverick", "Tango", "Atlas", "Ziggy", "Haze", "Knox",
+            "Drake", "Sable", "Bowie", "Rusty", "Cobalt", "Zane", "Hawk", "Talon", "Cruz", "Striker",
+            "Vapor", "Blitz", "Canyon", "Rush", "Gunner", "Stryker", "Panther", "Orion", "Spear", "Torque",
+            "Grit", "Saber", "Flash", "Iron", "Wolf", "Shade", "Rift", "Mako", "Jet", "Viking"
         ],
         female: [
             "Luna", "Bella", "Daisy", "Lucy", "Bailey", "Sadie", "Molly", "Stella", "Roxy", "Zoey",
             "Lily", "Lola", "Zoe", "Ruby", "Riley", "Ellie", "Nala", "Abby", "Chloe", "Pepper",
             "Rosie", "Willow", "Piper", "Sophie", "Maggie", "Coco", "Gracie", "Harper", "Penny", "Sasha",
             "Ivy", "Hazel", "Ginger", "Nova", "Olive", "Dixie", "Honey", "Millie", "Mia", "Winnie",
-            "Poppy", "Emma", "Lady", "Layla", "Kona", "Lexi", "Mila", "Phoebe", "Annie", "Maya"
+            "Poppy", "Emma", "Lady", "Layla", "Kona", "Lexi", "Mila", "Phoebe", "Annie", "Maya",
+            // New additions
+            "Tessa", "Cleo", "Poppy", "Jade", "Violet", "Aspen", "Cinnamon", "Sable", "Lulu", "Opal",
+            "Skye", "Fiona", "Ebony", "Phoebe", "Sienna", "Tinker", "Mabel", "Juniper", "Pearl", "Saffron",
+            "Nala", "Mimi", "Zara", "Raven", "Ivy", "Coco", "Dolly", "Bambi", "Tulip", "Fizzy",
+            "Chanel", "Velvet", "Sundae", "Blossom", "Tiffy", "Lacey", "Glimmer", "Starlight", "Coral", 
+            "Breezy", "Flora", "Pebbles", "Suki", "Wren", "Marigold", "Taffy", "Sable", "Zinnia", "Cali"
         ]
     },
     
@@ -75,19 +126,37 @@ const petNames = {
             "Milo", "Sophie", "Jack", "Nala", "Kitty", "Loki", "Smokey", "Tiger", "Oscar", "Shadow",
             "Mittens", "Felix", "Pepper", "Gracie", "Jasper", "Daisy", "Salem", "Cleo", "Rocky", "Penny",
             "Gizmo", "Misty", "Sam", "Zoe", "Boots", "Maggie", "Romeo", "Coco", "George", "Poppy",
-            "Teddy", "Ruby", "Scout", "Rosie", "Toby", "Piper", "Zeus", "Princess", "Louie", "Ellie"
+            "Teddy", "Ruby", "Scout", "Rosie", "Toby", "Piper", "Zeus", "Princess", "Louie", "Ellie",
+            // New additions
+            "Sammy", "Hazel", "Gus", "Ivy", "Atlas", "Rusty", "Tessa", "Maverick", "Jade", "Rex",
+            "Sasha", "Boomer", "Frankie", "Violet", "Dexter", "Pippin", "Aspen", "Tango", "Moose", "Cinnamon",
+            "Sable", "Mimi", "Zara", "Casper", "Lulu", "Bruno", "Opal", "Titan", "Skye", "Bowie",
+            "Fiona", "Ranger", "Ebony", "Phoebe", "Gideon", "Sienna", "Dash", "Tinker", "Mabel", "Arrow",
+            "Cairo", "Raven", "Juniper", "Odin", "Pearl", "Saffron", "Nala", "Gus", "Mimi", "Zara"
         ],
         male: [
             "Oliver", "Leo", "Charlie", "Max", "Simba", "Milo", "Jack", "Loki", "Tiger", "Oscar",
             "Shadow", "Felix", "Jasper", "Salem", "Rocky", "Gizmo", "Sam", "Boots", "Romeo", "George",
             "Teddy", "Scout", "Toby", "Zeus", "Louie", "Ollie", "Finn", "Winston", "Ziggy", "Oreo",
-            "Binx", "Archie", "Dexter", "Peanut", "Cooper", "Thomas", "Sammy", "Buster", "Murphy", "Tigger"
+            "Binx", "Archie", "Dexter", "Peanut", "Cooper", "Thomas", "Sammy", "Buster", "Murphy", "Tigger",
+            // New additions
+            "Ranger", "Boomer", "Frankie", "Pippin", "Titan", "Cairo", "Gideon", "Dash", "Odin", "Casper",
+            "Arrow", "Bruno", "Rex", "Sammy", "Maverick", "Tango", "Atlas", "Ziggy", "Haze", "Knox",
+            "Drake", "Sable", "Bowie", "Rusty", "Cobalt", "Zane", "Hawk", "Talon", "Cruz", "Striker",
+            "Vapor", "Blitz", "Canyon", "Rush", "Gunner", "Stryker", "Panther", "Orion", "Spear", "Torque",
+            "Grit", "Saber", "Flash", "Iron", "Wolf", "Shade", "Rift", "Mako", "Jet", "Viking"
         ],
         female: [
             "Luna", "Bella", "Lucy", "Lily", "Chloe", "Sophie", "Nala", "Kitty", "Smokey", "Pepper",
             "Gracie", "Daisy", "Cleo", "Penny", "Misty", "Zoe", "Maggie", "Coco", "Poppy", "Ruby",
             "Rosie", "Piper", "Princess", "Ellie", "Callie", "Willow", "Mittens", "Sasha", "Molly", "Stella",
-            "Lola", "Angel", "Kiki", "Nova", "Olive", "Hazel", "Cali", "Mia", "Ginger", "Abby"
+            "Lola", "Angel", "Kiki", "Nova", "Olive", "Hazel", "Cali", "Mia", "Ginger", "Abby",
+            // New additions
+            "Tessa", "Jade", "Violet", "Aspen", "Cinnamon", "Sable", "Lulu", "Opal", "Skye", "Fiona",
+            "Ebony", "Phoebe", "Sienna", "Tinker", "Mabel", "Juniper", "Pearl", "Saffron", "Mimi", "Zara",
+            "Raven", "Ivy", "Coco", "Dolly", "Bambi", "Tulip", "Fizzy", "Chanel", "Velvet", "Sundae",
+            "Blossom", "Tiffy", "Lacey", "Glimmer", "Starlight", "Coral", "Breezy", "Flora", "Pebbles", 
+            "Suki", "Wren", "Marigold", "Taffy", "Sable", "Zinnia", "Cali", "Nixie", "Fable", "Lyric"
         ]
     },
     
@@ -98,7 +167,12 @@ const petNames = {
             "Buddy", "Skye", "Phoenix", "Lola", "Coco", "Oliver", "Luna", "Kiki", "Polly", "Lucky",
             "Zazu", "Echo", "Ziggy", "Willow", "Peaches", "Pip", "Ginger", "Rocky", "Koko", "Sky",
             "Blueberry", "Sunshine", "Tiki", "Oscar", "Lemon", "Angel", "Jasper", "Daisy", "Bandit", "Ruby",
-            "Storm", "Milo", "Winnie", "Toby", "Olive", "Finn", "Roxy", "Scout", "Pepper", "Bean"
+            // New additions
+            "Star", "Flicker", "Tango", "Saffron", "Zephyr", "Coral", "Tinker", "Flame", "Opal", "Ivy",
+            "Sparrow", "Noodle", "Breeze", "Sable", "Crimson", "Pippin", "Dawn", "Mango", "Sienna", "Dash",
+            "Feather", "Nixie", "Blaze", "Tulip", "Cedar", "Onyx", "Puffy", "Sprout", "Coco", "Glint",
+            "Fable", "Chirpy", "Lark", "Jade", "Vivid", "Bouncy", "Slate", "Dusty", "Petal", "Swoop",
+            "Blossom", "Cairo", "Raven", "Juniper", "Pearl", "Saffron", "Mimi", "Zara", "Glimmer", "Lyric"
         ]
     },
     
@@ -109,7 +183,12 @@ const petNames = {
             "Neptune", "Gill", "Dory", "Sunny", "Marlin", "Wave", "Sharky", "Rainbow", "Flipper", "Pearl",
             "Aqua", "Shelly", "Flounder", "Ariel", "Sebastian", "Captain", "Luna", "Moby", "Jaws", "Scale",
             "Poseidon", "Jacques", "Cleo", "Blub", "Squirtle", "Triton", "Betta", "Guppy", "Noodle", "Swimmer",
-            "Echo", "River", "Cove", "Sandy", "Shimmer", "Sparkle", "Stream", "Tide", "Drift", "Marina"
+            // New additions
+            "Tide", "Drift", "Marina", "Sable", "Cobalt", "Spritz", "Lagoon", "Zephyr", "Glint", "Shimmer",
+            "Reef", "Saphira", "Pippin", "Sushi", "Tango", "Opal", "Mako", "Crimson", "Sienna", "Dash",
+            "Bloop", "Fizzy", "Coralina", "Nixie", "Flare", "Sable", "Vivid", "Puffy", "Sprout", "Coco",
+            "Glimmer", "Chirpy", "Jade", "Bouncy", "Slate", "Dusty", "Petal", "Swoop", "Blossom", "Cairo",
+            "Raven", "Juniper", "Pearl", "Saffron", "Mimi", "Zara", "Lyric", "Tinsel", "Spark"
         ]
     },
     
@@ -120,7 +199,12 @@ const petNames = {
             "Iggy", "Crush", "Drake", "Tiny", "Chompers", "Fang", "Gex", "Rango", "Turtle", "Echo",
             "Naga", "Komodo", "Pascal", "Yoshi", "Bowser", "Chameleon", "Skink", "Raptor", "Flash", "Emerald",
             "Noodle", "Cobra", "Python", "Hydra", "Medusa", "Zeus", "Apollo", "Kaa", "Viper", "Snappy",
-            "Toothless", "Mushu", "Puff", "Biscuit", "Pebbles", "Rocky", "Shell", "Tortuga", "Slider", "Shelly"
+            // New additions
+            "Sable", "Titan", "Cairo", "Sienna", "Dash", "Tinker", "Mabel", "Juniper", "Odin", "Pearl",
+            "Saffron", "Mimi", "Zara", "Glimmer", "Lyric", "Obsidian", "Slate", "Crimson", "Vivid", "Pippin",
+            "Nixie", "Blaze", "Tulip", "Cedar", "Onyx", "Puffy", "Sprout", "Coco", "Glint", "Fable",
+            "Chirpy", "Jade", "Bouncy", "Dusty", "Petal", "Swoop", "Blossom", "Raven", "Mako", "Zephyr",
+            "Coral", "Flame", "Opal", "Ivy", "Sparrow", "Noodle", "Breeze", "Dawn", "Mango", "Viper"
         ]
     },
     
@@ -131,7 +215,12 @@ const petNames = {
             "Nugget", "Pepper", "Biscuit", "Noodle", "Charlie", "Button", "Marshmallow", "Fluffy", "Bean", "Teddy",
             "Pipsqueak", "Pebbles", "Chip", "Cookie", "Daisy", "Smokey", "Snickers", "Socks", "Thumper", "Cotton",
             "Midnight", "Snowball", "Maple", "Penny", "Rusty", "Shadow", "Ginger", "Buddy", "Speck", "Dusty",
-            "Mochi", "Mocha", "Benny", "Lulu", "Pumpkin", "Scout", "Bear", "Brownie", "Pepper", "Honey"
+            // New additions
+            "Mochi", "Mocha", "Benny", "Lulu", "Pumpkin", "Scout", "Bear", "Brownie", "Pepper", "Honey",
+            "Sammy", "Hazel", "Gus", "Ivy", "Atlas", "Cleo", "Rusty", "Tessa", "Maverick", "Jade",
+            "Rex", "Sasha", "Boomer", "Poppy", "Frankie", "Violet", "Dexter", "Pippin", "Aspen", "Tango",
+            "Moose", "Cinnamon", "Sable", "Mimi", "Zara", "Casper", "Lulu", "Bruno", "Opal", "Titan",
+            "Skye", "Bowie", "Fiona", "Ranger", "Ebony", "Phoebe", "Gideon", "Sienna", "Dash", "Tinker"
         ]
     }
 };
